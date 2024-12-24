449fbbf1d6c337a4a438802ca83490cc50be4589
xayam <xayam@yandex.ru>
2024-12-24 10:09:57 +0300

    v0.282

--- h/model/barriers/square/square4animation.py
+++ h/model/barriers/square/square4animation.py
@@ -6,7 +6,10 @@ from h.model.utils import utils_progress
 width = 1024
 height = 512
 square1line = Square1Line()
-font = ImageFont.truetype(font="arial.ttf", size=32)
+font = ImageFont.truetype(
+    font="arial.ttf",
+    size=32
+)
 images = []
 borders = [
     ["red", "green", "blue", "yellow"],
@@ -24,8 +27,11 @@ for j in range(4):
     for i in range(0, height):
         if i % 32 != 0:
             continue
-        images.append(Image.new(mode="RGBA", size=(width, height),
-                                color=(255, 255, 255)))
+        images.append(Image.new(
+            mode="RGBA",
+            size=(width, height),
+            color=(255, 255, 255))
+        )
         draw = ImageDraw.Draw(images[-1])
         filename = f"frames4animation/{j}{str(i).rjust(3, '0')}.png"
         square = [
@@ -36,12 +42,18 @@ for j in range(4):
         ]
         for x in range(0, height, 64):
             for y in range(0, height, 64):
-                draw.rectangle(xy=[(x, y), (x + 64, y + 64)],
-                               fill=(64 + x//4, 64 + y//4, 64 + x//4),
-                               outline="black", width=1)
-                draw.text(xy=(x + 15, y + 15),
-                          text=str(y//64 * 8 + x//64).rjust(2, "0"),
-                          font=font, fill="black")
+                draw.rectangle(
+                    xy=[(x, y), (x + 64, y + 64)],
+                    fill=(64 + x//4, 64 + y//4, 64 + x//4),
+                    outline="black",
+                    width=1
+                )
+                draw.text(
+                    xy=(x + 15, y + 15),
+                    text=str(y//64 * 8 + x//64).rjust(2, "0"),
+                    font=font,
+                    fill="black"
+                )
         draw.line(xy=square[0], fill=borders[j][0], width=2)
         draw.line(xy=square[3], fill=borders[j][1], width=2)
         draw.line(xy=square[1], fill=borders[j][2], width=2)
@@ -62,7 +74,10 @@ for j in range(4):
         k = 0
         for index1 in range(len(dists[0])):
             distances1 = square1line.get_distances(
-                x1=0.0, y1=0.0, x2=dists[0][index1][0], y2=dists[1][index1][0],
+                x1=0.0,
+                y1=0.0,
+                x2=dists[0][index1][0],
+                y2=dists[1][index1][0],
                 grid=square1line.grid[64]
             )
             if distances1 is None:
@@ -70,7 +85,10 @@ for j in range(4):
             distances1 = square1line.dim1_to_dim2(distances1)
             distances1 = square1line.dim2_to_dim1(distances1)
             distances2 = square1line.get_distances(
-                x1=0.0, y1=0.0, x2=dists[2][index1][0], y2=dists[3][index1][0],
+                x1=0.0,
+                y1=0.0,
+                x2=dists[2][index1][0],
+                y2=dists[3][index1][0],
                 grid=square1line.grid[64]
             )
             if distances2 is None:
@@ -88,9 +106,14 @@ for j in range(4):
                     xy=(x, y),
                     fill=(r, g, r),
                 )
-        images[-1].save(filename, format="PNG")
+        images[-1].save(
+            fp=filename,
+            format="PNG"
+        )
 images[0].save(
-    "square4animation.gif",
+    fp="square4animation.gif",
     save_all=True,
-    append_images=images[1:], duration=height, loop=0
+    append_images=images[1:],
+    duration=height,
+    loop=0
 )
