45a441802f58f971ed18fef255f5e40f04d81768
xayam <xayam@yandex.ru>
2024-12-24 19:58:26 +0300

    v0.299 square animation

--- h/model/barriers/square/square1line.py
+++ h/model/barriers/square/square1line.py
@@ -57,7 +57,7 @@ class Square1Line:
             ]
         except ZeroDivisionError:
             print(
-                f"[INFO] ZeroDivisionError        | "
+                f"[INFO] ZeroDivisionError         | "
                 f"x1={x1} | y1={y1} | x2={x2} | y2={y2}"
             )
             return None
