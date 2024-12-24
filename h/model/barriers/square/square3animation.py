f9baf3e0a2184086d290e5baaae0f7771b728e1f
xayam <xayam@yandex.ru>
2024-12-24 19:03:05 +0300

    v0.292 square animation

--- h/model/barriers/square/square3animation.py
+++ h/model/barriers/square/square3animation.py
@@ -4,8 +4,8 @@ from h.model.barriers.square.square1line import Square1Line
 
 width = 1024
 height = 512
-step = 1
-# step = 32
+# step = 1
+step = 32
 
 square1line = Square1Line()
 font = ImageFont.truetype(
@@ -14,6 +14,7 @@ font = ImageFont.truetype(
 )
 frames = []
 borders = ["red", "green", "blue"]
+indexes = [[0, 1], [1, 2], [2, 0]]
 for j in range(4):
     for i in range(0, height, step):
         frame = Image.new(
@@ -79,45 +80,26 @@ for j in range(4):
         for z in range(len(distances)):
             distances[z] = square1line.dim1_to_dim2(distances[z])
         for index1 in range(len(distances[0])):
-            distance1 = square1line.get_distances(
-                x1=0.0,
-                y1=0.0,
-                x2=distances[0][index1][0],
-                y2=distances[1][index1][0],
-                grid=square1line.grid[64]
-            )
-            if distance1 is None:
+            distance = []
+            for z in range(3):
+                distance.append(square1line.get_distances(
+                    x1=0.0,
+                    y1=0.0,
+                    x2=distances[indexes[z][0]][index1][0],
+                    y2=distances[indexes[z][1]][index1][0],
+                    grid=square1line.grid[64]
+                ))
+            if None in distance:
                 continue
-            distance1 = square1line.dim1_to_dim2(distance1)
-            distance1 = square1line.dim2_to_dim1(distance1)
-            distance2 = square1line.get_distances(
-                x1=0.0,
-                y1=0.0,
-                x2=distances[1][index1][0],
-                y2=distances[2][index1][0],
-                grid=square1line.grid[64]
-            )
-            if distance2 is None:
-                continue
-            distance2 = square1line.dim1_to_dim2(distance2)
-            distance2 = square1line.dim2_to_dim1(distance2)
-            distance3 = square1line.get_distances(
-                x1=0.0,
-                y1=0.0,
-                x2=distances[2][index1][0],
-                y2=distances[0][index1][0],
-                grid=square1line.grid[64]
-            )
-            if distance3 is None:
-                continue
-            distance3 = square1line.dim1_to_dim2(distance3)
-            distance3 = square1line.dim2_to_dim1(distance3)
-            for index2 in range(len(distance1)):
-                t = round(256 * (2 * distance3[index2] + 1) / 2)
+            for z in range(3):
+                distance[z] = square1line.dim1_to_dim2(distance[z])
+                distance[z] = square1line.dim2_to_dim1(distance[z])
+            for index2 in range(len(distance[0])):
+                t = round(256 * (2 * distance[2][index2] + 1) / 2)
                 r = t % 256
                 g = t // 256
-                x = height + round(256 * (2 * distance1[index2] + 1))
-                y = round(256 * (2 * distance2[index2] + 1))
+                x = height + round(256 * (2 * distance[0][index2] + 1))
+                y = round(256 * (2 * distance[1][index2] + 1))
                 canvas.point(
                     xy=(x, y),
                     fill=(r, g, r),
