5afece3f87641ef16a582374462a6717978b5412
xayam <xayam@yandex.ru>
2025-01-25 11:08:26 +0300

    v0.309

--- h/model/barriers/antenna/data.py
+++ h/model/barriers/antenna/data.py
@@ -1,23 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+
 SIZE = 16
-def primfacs(n):
-   i = 2
-   primfac = []
-   while i * i <= n:
-       while n % i == 0:
-           primfac.append(i)
-           n = n / i
-       i = i + 1
-   if n > 1:
-       primfac.append(round(n))
-   return primfac
+
+
+def primfacs(p):
+    i = 2
+    primfac = []
+    while i * i <= p:
+        while p % i == 0:
+            primfac.append(i)
+            p = p / i
+        i = i + 1
+    if p > 1:
+        primfac.append(round(p))
+    return primfac
+
+
 data = [[0, 0, 0], [1, 1, 1]]
 for n in range(2, SIZE):
     factor = primfacs(n)
     if len(factor) in [1, 2, 3]:
         tail = [1] * (3 - len(factor))
         data.append(tail + factor)
+
+for x, y, z in data:
+    print(f"[{x}, {y}, {z}]")
+
 data = np.asarray(data)
 fig = plt.figure(figsize=(12, 12))
 ax = fig.add_subplot(projection='3d')
