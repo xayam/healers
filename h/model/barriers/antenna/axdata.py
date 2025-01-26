5c1c93c56598921f45c7542987b92390c981ca04
xayam <xayam@yandex.ru>
2025-01-26 18:34:12 +0300

    v0.316 AX

--- h/model/barriers/antenna/axdata.py
+++ h/model/barriers/antenna/axdata.py
@@ -1,14 +1,50 @@
+import os.path
+from os import mkdir
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-SIZES = [16, 128]
+PARAMS = [
+    [
+        16, # size
+        "1/71", # scale
+        "0.0001", # radius
+        "1420" # freq MHz
+    ],
+    [
+        16, # size
+        "1/27", # scale
+        "0.0001", # radius
+        "432" # freq MHz
+    ],
+    [
+        64, # size
+        "1/71", # scale
+        "0.0001", # radius
+        "1420" # freq MHz
+    ],
+
+]
 
 
 class AXData:
-    def __init__(self, size, plot=True):
+    def __init__(self, size, scale, radius, freq_mhz, plot=True):
+        self.output = None
+        self.template = None
+        self.data = None
         self.size = size
+        self.scale = scale
+        self.radius = radius
+        self.freq_mhz = freq_mhz
+        self.folder = f"ax{self.size}"
+        self.filename = f"{self.folder}/AX{self.size}_{self.freq_mhz}MHz.nec"
         self.plot = plot
+        self.init()
+
+    def init(self):
+        with open("template.nec", mode="r", encoding="utf-8") as template:
+            self.template = template.read()
 
     @staticmethod
     def primfacs(p):
@@ -30,14 +66,19 @@ class AXData:
         print("[1, 1, 1]")
         for n in range(2, self.size):
             factor = self.primfacs(n)
-            if len(factor) in [1, 2, 3]:
-                tail = [1] * (3 - len(factor))
-                data.append(tail + factor)
-                x, y, z = data[-1]
-                print(f"[{x}, {y}, {z}]")
-            else:
-                print(n)
+            x, y, z = 0, 0, 0
+            for f in range(0, len(factor), 3):
+                tail = factor[f: f + 3]
+                head = [1] * (3 - len(tail))
+                chunk = head + tail
+                chunk[0], chunk[1], chunk[2] = \
+                    chunk[0] - x, chunk[1] - y, chunk[2] - z
+                data.append(chunk)
+                print(f"[{chunk[0]}, {chunk[1]}, {chunk[2]}]")
+                # print(f"n={n}, head={head}, tail={tail}")
+                x, y, z = chunk[0], chunk[1], chunk[2]
         print(f"SIZE={self.size}, len(data)={len(data)}")
+        self.data = data
         if self.plot:
             data = np.asarray(data)
             fig = plt.figure(figsize=(12, 12))
@@ -45,11 +86,44 @@ class AXData:
             ax.plot(data[:, 0], data[:, 1], data[:, 2])
             plt.show()
 
+    def nec(self):
+        if not os.path.exists(self.folder):
+            mkdir(self.folder)
+        self.output = str(self.template).replace(
+            "{{SCALE}}", self.scale, 1
+        )
+        self.output = str(self.output).replace(
+            "{{RADIUS}}", self.radius, 1
+        )
+        self.output = str(self.output).replace(
+            "{{FREQMHZ}}", self.freq_mhz, 1
+        )
+        x, y, z = 0, 0, 0
+        self.data.append([x, y, z])
+        gws = ""
+        segment = 2
+        for i in range(1, len(self.data)):
+            gws += \
+                f"GW {i} {segment} {x}*S {y}*S {z}*S " \
+                f"{self.data[i][0]}*S {self.data[i][1]}*S {self.data[i][2]}*S R\n"
+            x, y, z = self.data[i][0], self.data[i][1], self.data[i][2]
+        self.output = str(self.output).replace(
+            "{{GW}}", gws[:-1], 1
+        )
+        with open(self.filename, mode="w", encoding="utf-8") as output:
+            output.write(self.output)
+
 
 def main():
-    for size in SIZES:
-        ax = AXData(size=size)
+    for size, scale, radius, freq_mhz in PARAMS:
+        ax = AXData(
+            size=size,
+            scale=scale,
+            radius=radius,
+            freq_mhz=freq_mhz
+        )
         ax.run()
+        ax.nec()
 
 
 if __name__ == "__main__":
