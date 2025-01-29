bccc42edbd1d73e5434cffd1441f64a8f41a91bb
xayam <xayam@yandex.ru>
2025-01-29 22:28:23 +0300

    v0.319

--- h/model/barriers/mind/hyper.py
+++ h/model/barriers/mind/hyper.py
@@ -1,22 +1,19 @@
 import json
 import os
+import sys
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-class AXData:
+class Hyper:
     def __init__(self, limit, plot=True):
         self.output = None
-        self.template = None
         self.data = None
         self.limit = limit
         self.plot = plot
-        self.init()
 
-    def init(self):
-        with open("template.nec", mode="r", encoding="utf-8") as template:
-            self.template = template.read()
+
 
     @staticmethod
     def primfacs(p):
@@ -34,17 +31,18 @@ class AXData:
     def run(self):
         data = [[[0, 0, 0], [1, 1, 1]]]
         n = 2
-        t = 6
+        t = 0
         while True:
             factor = self.primfacs(n)
+            # factor = [1] * (9 - len(factor)) + factor
             i = 0
             for f in range(0, len(factor), 3):
                 tail = factor[f: f + 3]
                 head = [1] * (3 - len(tail))
                 chunk = head + tail
+                # chunk = factor[f: f + 3]
                 try:
                     data[i].append(chunk)
-
                 except IndexError:
                     data.append([])
                     data[i].append(chunk)
@@ -53,9 +51,6 @@ class AXData:
                     message += f"{len(k)}-"
                 message = message[:-1] + f", n={n}, i={i}, chunk={chunk}"
                 print(message)
-                # with open("log.txt", mode="a", encoding="utf-8") as log:
-                #     log.write(message + "\n")
-
                 i += 1
             try:
                 if len(data[t + 2]) >= self.limit:
@@ -66,12 +61,18 @@ class AXData:
         for d in data:
             print(f"len(d)={len(d)}")
         data1 = [
-            data[t][:self.limit],
-            data[t + 1][:self.limit],
-            data[t + 2][:self.limit]
+            data[t][: self.limit],
+            data[t + 1][: self.limit],
+            data[t + 2][: self.limit]
         ]
+        # for d in data1:
+        #     print(d)
+        # sys.exit()
         data = [
             [
+                # data1[0][j][0] + data1[1][j][0] + data1[2][j][0],
+                # data1[0][j][1] + data1[1][j][1] + data1[2][j][1],
+                # data1[0][j][2] + data1[1][j][2] + data1[2][j][2]
                 data1[0][j][0] * data1[0][j][1] * data1[0][j][2],
                 data1[1][j][0] * data1[1][j][1] * data1[1][j][2],
                 data1[2][j][0] * data1[2][j][1] * data1[2][j][2]
@@ -104,9 +105,11 @@ class AXData:
         self.data = []
         for a in appendix:
             self.data += a
-        with open(f"data{t}.json", mode="w", encoding="utf-8") as js:
-            js.write(json.dumps(self.data))
+        # with open(f"data{t}.json", mode="w", encoding="utf-8") as js:
+        #     js.write(json.dumps(self.data))
         data = self.data
+        # for d in data:
+        #     print(d)
         if self.plot:
             data = np.asarray(data)
             fig = plt.figure(figsize=(12, 12))
@@ -114,47 +117,10 @@ class AXData:
             ax.plot(data[:, 0], data[:, 1], data[:, 2])
             plt.show()
 
-    def nec(self):
-        folder = "ax"
-        scale = "1/142"
-        radius = "0.00001"
-        freq_mhz = "1420"
-        filename = f"{folder}/AX_{freq_mhz}MHz.nec"
-        if not os.path.exists(folder):
-            os.mkdir(folder)
-        self.output = str(self.template).replace(
-            "{{SCALE}}", scale, 1
-        )
-        self.output = str(self.output).replace(
-            "{{RADIUS}}", radius, 1
-        )
-        self.output = str(self.output).replace(
-            "{{FREQMHZ}}", freq_mhz, 1
-        )
-        x, y, z = 0, 0, 0
-        # self.data.append([x, y, z])
-        gws = ""
-        segment = 2
-        for i in range(1, len(self.data)):
-            gws += \
-                f"GW {i} {segment} {x}*S {y}*S {z}*S " \
-                f"{self.data[i][0]}*S {self.data[i][1]}*S {self.data[i][2]}*S R\n"
-            x, y, z = self.data[i][0], self.data[i][1], self.data[i][2]
-        self.output = str(self.output).replace(
-            "{{GW}}", gws[:-1], 1
-        )
-        with open(filename, mode="w", encoding="utf-8") as output:
-            output.write(self.output)
-
 
 def main():
-    ax = AXData(limit=23)
-    ax.run()
-    # ax = AXData(limit=22)
-    # ax.run()
-    # ax = AXData(limit=32)
-    # ax.run()
-    # ax.nec()
+    h = Hyper(limit=16)
+    h.run()
 
 
 if __name__ == "__main__":
